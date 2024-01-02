6450176089fce4157b7edcdb83e8de4aa01f85be
TuNguyen(Shin) <tnmrtupro1@gmail.com>
2024-01-02 16:32:43 +0700

    Refactor Authentication

---
+++ a1a-style-master/src/test/java/com/allianceoneapparel/A1aStyleMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianceoneapparel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1aStyleMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
